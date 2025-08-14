24d3edf26c4c295d532d096124ccf5bc98cbc306
shayan-1425 <shayanghosh1425@gmail.com>
2025-08-14 08:51:04 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,15 +12,7 @@ public class Main implements Directions {
     World.setSize(10,10);
     World.setDelay(100);
 
-		karel.move();
-    karel.putBeeper();
-    karel.move();
-    karel.move();
-    karel.turnLeft();
-
-    karel.move();
-    karel.putBeeper();
-    karel.putBeeper();
+	karel.putBeeper();
 
   }
     
